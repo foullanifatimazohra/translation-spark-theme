12b54469bd54723d3f6ebd0935b89f29b0b56b4c
foullanifatimazohra <foullanifatoum@gmail.com>
2023-11-04 12:09:51 +0100

    feat : add dir to handle RTL in arabic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
 import Loader from "./components/ui/Loader";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { i18n } = useTranslation();
+  useEffect(() => {
+    const dir = i18n.dir(i18n.language);
+    document.documentElement.dir = dir;
+  }, [i18n, i18n.language]);
   return (
     <div className="App">
       <Loader />
